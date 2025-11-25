48131aba61e7357edf1e6f47419eb55b54955f1c
mcht67 <malte.crt@googlemail.com>
2025-11-25 13:12:26 +0100

    adds step: analyze audio

--- source/make_dataset.py
+++ source/make_dataset.py
@@ -1,13 +1,16 @@
-import separate_audio, load_datasets
+import load_datasets, separate_audio, analyze_audio
 #from source.pipeline import load_datasets
 
 def main():
 
     # Load datasets
-    load_datasets.main()
+    #load_datasets.main()
 
     # Separate audio
     separate_audio.main()
+
+    # Analyze audio with birdnetlib
+    analyze_audio.main()
    
 
 if __name__ == '__main__':
